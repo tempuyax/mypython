c6aee1f00748762cb85817c9936568d7b2cbd45d
tempuyax <tempuyax@gmail.com>
2022-11-05 18:37:05 +0800

    new theodolit.py

---
+++ theodolit.py
@@ -0,0 +1,155 @@
+# Menghitung data ukur theodolit
+# Pahor.m @Oktober 2022
+# python via termux
+import math
+
+# Coloring text mode
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    OKYELLOW = '\033[33m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Function Angle 360 Controling
+def sign_360(Azimut):
+    if Azimut > 0:
+        return Azimut       # 1 or Positive Angle
+    elif Azimut == 0:
+        return 0.0          # 0 or Zero Angle
+    else:
+        return Azimut + 360 # -1 or Negative Angle
+
+print (bcolors.BOLD,'#BasePoint#',bcolors.ENDC)
+# input East
+while True:
+    try:
+        bN = float(input("N: "))
+        #print ("You entered: ",BaseEast)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+# input North
+while True:
+    try:
+        bE = float(input("E: "))
+        #print ("You entered: ",BaseNorth)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+# input Zenith
+while True:
+    try:
+        bZ = float(input("Z: "))
+        #print ("You entered: ",BaseNorth)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+#print ('East ' + str(BaseEast) + ',' + 'North ' + str(BaseNorth))
+
+#≠==========÷=========================
+print (bcolors.BOLD,'#Instrument Theodolit#',bcolors.ENDC)
+
+# input East
+while True:
+    try:
+        HI = float(input("HI: "))
+        # print ("You entered: ",TargetEast)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+0# input East
+while True:
+    try:
+        BT = float(input("BT: "))
+        # print ("You entered: ",TargetEast)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+# input North
+while True:
+    try:
+        BA = float(input("BA: "))
+#        print ("You entered: ",TargetNorth)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+while True:
+    try:
+        VA = float(input("VA (DD.MMSS): "))
+        #print (VA)
+        Dd = int(VA)
+        #if Dd <= 360:
+        VA = ((VA-Dd)*100)
+        #else:
+           #except ValueError:
+           #print (VA)
+        Md = int(VA)
+        VA = ((VA-Md)*100)
+        #print (VA)
+        Sd = round(VA)
+        #print ("You entered: ",Dd," ",Md," ",Sd)
+        #print ("Decimal VA : ",Dd+(Md/60)+(Sd/3600))
+        VA = Dd+(Md/60)+(Sd/3600)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+while True:
+    try:
+        HA = float(input("HA (DD.MMSS): "))
+        #print (HA)
+        Dd = int(HA)
+        HA = ((HA-Dd)*100)
+        #print (HA)
+        Md = int(HA)
+        HA = ((HA-Md)*100)
+        #print (HA)
+        Sd = round(HA)
+        #print ("You entered: ",Dd," ",Md," ",Sd)
+        #print ("Decimal HA : ",Dd+(Md/60)+(Sd/3600))
+        HA = Dd+(Md/60)+(Sd/3600)
+        break;
+    except ValueError:
+        print ("Invalid input")
+
+
+#print ('East ' + str(TargetEast) + ',' + 'North ' + str(TargetNorth))
+
+# Main Calculation of Polar coordinat
+SD = (BA-BT)*200
+HD = math.sin(math.radians(VA))*SD
+
+dN = (SD * math.sin(math.radians(VA))) * math.cos(math.radians(HA))
+dE = (SD * math.sin(math.radians(VA))) * math.sin(math.radians(HA))
+dZ = (SD * math.cos(math.radians(VA))) + HI - BT
+
+N = bN + dN
+E = bE + dE
+Z = bZ + dZ
+
+print (bcolors.BOLD,'#Distance Point#',bcolors.ENDC)
+print ("SD: ",round(SD,3))
+print ("HD: ",round(HD,3))
+
+print (bcolors.BOLD,'#Delta Point#',bcolors.ENDC)
+print ("dN:",round(dN,3))
+print ("dE:",round(dE,3))
+print ("dZ:",round(dZ,3))
+
+print (bcolors.BOLD,'#Target Point#',bcolors.ENDC)
+print ("N:",round(N,3))
+print ("E:",round(E,3))
+print ("Z:",round(Z,3))
+
